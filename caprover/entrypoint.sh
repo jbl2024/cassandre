2be8eae97becb94e3a9699e8c65b982443a1cbbb
Jérôme Blondon <jerome@blondon.fr>
2023-04-29 22:03:25 +0200

    Keep python

--- caprover/entrypoint.sh
+++ caprover/entrypoint.sh
@@ -4,13 +4,13 @@
 cd /app
 
 # Run migrations
-python3 manage.py migrate
+python manage.py migrate
 
 # Create superuser if it does not exist
-python3 manage.py create_superuser
+python manage.py create_superuser
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Start Gunicorn
 gunicorn cassandre.wsgi:application \
