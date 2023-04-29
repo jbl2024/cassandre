d9e46072bae197eb01813c849d0ac11d6d09df9b
Jérôme Blondon <jerome@blondon.fr>
2023-04-29 21:35:42 +0200

    Add gunicorn, use python3

--- caprover/entrypoint.sh
+++ caprover/entrypoint.sh
@@ -4,13 +4,13 @@
 cd /app
 
 # Run migrations
-python manage.py migrate
+python3 manage.py migrate
 
 # Create superuser if it does not exist
-python manage.py create_superuser
+python3 manage.py create_superuser
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Start Gunicorn
 gunicorn cassandre.wsgi:application \
