a41df380cb211a9580c3b6534b494e55a94c257e
Jérôme Blondon <jerome@blondon.fr>
2023-04-29 15:43:26 +0200

    WIP

--- documents/management/commands/index_documents.py
+++ documents/management/commands/index_documents.py
@@ -3,8 +3,10 @@ from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredFileLoader
-
+from langchain.embeddings import HuggingFaceEmbeddings 
 from documents.models import Document
+import os
+import shutil
 
 class Command(BaseCommand):
     help = 'Index all documents in the Document app, create embeddings and save them in Chroma vector DB'
@@ -14,19 +16,33 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         persist_directory = options['persist_directory']
+        # Create persist_directory if it doesn't exist
+        if not os.path.exists(persist_directory):
+            os.makedirs(persist_directory)
+        # Delete all files inside persist_directory if it exists
+        else:
+            for filename in os.listdir(persist_directory):
+                file_path = os.path.join(persist_directory, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    self.stdout.write(self.style.ERROR(f'Failed to delete {file_path}. Reason: {str(e)}'))
 
         documents = Document.objects.all()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
         docs = []
         for document in documents:
             loader = UnstructuredFileLoader(document.file.path, mode="elements")
             loaded_documents = loader.load()
             docs.extend(loaded_documents)
-            self.stdout.write(self.style.SUCCESS(f'Successfully indexed document: {document.title or document.file}'))
+            self.stdout.write(self.style.SUCCESS(f'Successfully loaded document: {document.title or document.file}'))
 
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=4000, chunk_overlap=200)
         texts = text_splitter.split_documents(docs)
 
-        embeddings = OpenAIEmbeddings()
-        docsearch = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory)
+        embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        Chroma.from_documents(texts, embeddings, persist_directory=persist_directory)
 
--- documents/search.py
+++ documents/search.py
@@ -1,21 +1,30 @@
 # documents/search.py
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from langchain.embeddings import HuggingFaceEmbeddings
 
-def search_documents(query, persist_directory='chroma_db'):
-    embeddings = OpenAIEmbeddings()
-    docsearch = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
 
+def search_documents(query, persist_directory="chroma_db_st"):
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    docsearch = Chroma(
+        persist_directory=persist_directory, embedding_function=embeddings
+    )
 
     prompt = PromptTemplate(
         input_variables=["question"],
         template="Je suis un robot d'aide pour les enseignants qui participent au mouvement inter-académique, je réponds en français. La question est la suivante: {question}?",
     )
 
-    qa = RetrievalQAWithSourcesChain.from_chain_type(OpenAI(temperature=0, model_name="gpt-3.5-turbo"), chain_type="stuff", retriever=docsearch.as_retriever())
-    results = qa({"question": prompt.format(question=query)}, return_only_outputs=True)
+    qa = RetrievalQA.from_chain_type(
+        llm=ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo"),
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True
+    )
+
+    results = qa({"query": prompt.format(question=query)}, return_only_outputs=False)
     print(results)
     return results
