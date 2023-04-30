6824eb942ce40002c8eaef51ff3f38fd47514997
Jérôme Blondon <jerome@blondon.fr>
2023-04-30 18:49:54 +0200

    Set current time

--- documents/search.py
+++ documents/search.py
@@ -5,6 +5,7 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from datetime import datetime
 import qdrant_client
 
 def search_documents(query, engine="gpt-3.5-turbo"):
@@ -18,17 +19,22 @@ def search_documents(query, engine="gpt-3.5-turbo"):
         embedding_function=embeddings.embed_query
     )    
 
+    now = datetime.now()
+
+    # Formater la date et l'heure en français
+    formatted_date_time = now.strftime("%d %B %Y à %H:%M")
+
     prompt = PromptTemplate(
         input_variables=["question"],
-        template="""
+        template=f"""
+        Nous sommes le {formatted_date_time}
         Tu es un un robot d'aide pour les enseignants qui participent au mouvement inter-académique, 
         tu réponds en français au féminin. Ton nom est Cassandre.
         Si dans ta réponse tu vas parler des syndicats, tu réponds que tu ne sais pas.
         Si tu ne connais pas la réponse, tu n'inventes rien. 
-        La question est la suivante: {question}
+        La question est la suivante: {{question}}
         """,
     )
-
     qa = RetrievalQA.from_chain_type(
         llm=ChatOpenAI(temperature=0, model_name=engine),
         chain_type="stuff",
