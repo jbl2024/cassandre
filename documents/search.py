bd9bd47f91dca567256443b8a15fe496f7174236
Jérôme Blondon <jerome@blondon.fr>
2023-05-01 09:30:03 +0200

    Add local context

--- documents/search.py
+++ documents/search.py
@@ -24,17 +24,23 @@ def search_documents(query, history, engine="gpt-3.5-turbo"):
     # Formater la date et l'heure en français
     formatted_date_time = now.strftime("%d %B %Y à %H:%M")
 
+    res = docsearch.similarity_search(query, k=4)
+    context = ""
+    for doc in res:
+        context = f"{context}\n{doc.page_content}"
     prompt = PromptTemplate(
-        input_variables=["question", "history"],
+        input_variables=["question", "context"],
         template=f"""
         Nous sommes le {formatted_date_time}
         Tu es un un robot qui accompagne les gestionnaires qui gèrent le mouvement inter-académique des enseignants. 
         Tu réponds en français au féminin. Ton nom est Cassandre.
+        Tu n'as pas besoin de dire bonjour, ni de préciser que tu es un robot d'accompagnement.
         Si tu ne connais pas la réponse, tu n'inventes rien et tu suggère de contacter le responsable du mouvement. 
-        L'historique des échanges est le suivant: {{history}}.
+        Le context que tu connais est le suivant: {{context}}.
         La question est la suivante: {{question}}
         """,
     )
+
     qa = RetrievalQA.from_chain_type(
         llm=ChatOpenAI(temperature=0, model_name=engine),
         chain_type="stuff",
@@ -43,6 +49,5 @@ def search_documents(query, history, engine="gpt-3.5-turbo"):
         verbose=True
     )
 
-    results = qa({"query": prompt.format(history=history, question=query)}, return_only_outputs=True)
-    print(results)
+    results = qa({"query": prompt.format(context=context, question=query)}, return_only_outputs=True)
     return results
