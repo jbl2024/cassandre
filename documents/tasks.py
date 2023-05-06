48e79a6e050fefdf61174dba55e92835bfe257c2
Jérôme Blondon <jerome@blondon.fr>
2023-05-06 20:01:05 +0200

    Fix task

--- documents/tasks.py
+++ documents/tasks.py
@@ -1,6 +1,6 @@
 from celery import shared_task
-from documents.index import index_documents
+from documents import index
 
 @shared_task
 def index_documents():
-    index_documents()
+    index.index_documents()
