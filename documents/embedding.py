e6f18c981b665378803bcb397059ec94813ec54f
Jérôme Blondon <jerome@blondon.fr>
2023-05-06 20:00:43 +0200

    OpenAI embeddings

--- documents/embedding.py
+++ documents/embedding.py
@@ -1,5 +1,6 @@
 from langchain.embeddings import OpenAIEmbeddings
-
+from langchain.embeddings import SentenceTransformerEmbeddings 
 
 def get_embedding():
+    # return SentenceTransformerEmbeddings(model_name="sentence-transformers/multi-qa-MiniLM-L6-cos-v1")
     return OpenAIEmbeddings()
