30d8f607f3302be7e8b339a9df36f1b558d5f031
Jérôme Blondon <jerome@blondon.fr>
2023-05-22 20:59:57 +0200

    Use daphne in prod

--- caprover/entrypoint.sh
+++ caprover/entrypoint.sh
@@ -15,11 +15,5 @@ python manage.py create_superuser
 # Start Celery worker in the background
 celery -A cassandre worker --loglevel=info &
 
-# Start gunicorn
-gunicorn cassandre.asgi:application \
-    -k uvicorn.workers.UvicornWorker \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --timeout 600 \
-    --access-logfile '-' \
-    --error-logfile '-'
+# Start Daphne
+daphne cassandre.asgi:application
