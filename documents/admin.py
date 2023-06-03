44e176bd3243d8c4d21d5193d93acba53297f4a5
Jérôme Blondon <jerome@blondon.fr>
2023-06-03 11:15:57 +0200

    Add download all files action

--- documents/admin.py
+++ documents/admin.py
@@ -1,38 +1,63 @@
+import os
+from tempfile import TemporaryDirectory, TemporaryFile
+from zipfile import ZipFile
+
 from django import forms
 from django.contrib import admin
-from django.db import models
-from django.urls import reverse
-from django.utils.http import urlencode
-
-from .models import Category, Document, Correction
+from django.http import FileResponse
+from django.core.files.storage import default_storage
+from tempfile import TemporaryDirectory
+from zipfile import ZipFile
+from tempfile import TemporaryFile
+from django.http import FileResponse
+from .models import Category, Correction, Document
 from .tasks import index_documents, index_documents_in_category
+import re
 
+
+def sanitize_filename(filename):
+    """
+    Sanitize a filename by removing characters that could cause issues.
+    """
+    return re.sub(r'(?u)[^-\'’ \w.]', '', filename.strip())
 
 def index_documents_action(modeladmin, request, queryset):
     index_documents.delay()
-    modeladmin.message_user(request, 'Documents indexing has started, please wait for completion')
-index_documents_action.short_description = 'Index selected documents'
+    modeladmin.message_user(
+        request, "Documents indexing has started, please wait for completion"
+    )
+
+
+index_documents_action.short_description = "Index selected documents"
+
 
 def index_documents_in_category_action(modeladmin, request, queryset):
     for category in queryset:
-        index_documents_in_category.delay(category.id) 
-    modeladmin.message_user(request, 'Documents indexing has started, please wait for completion')
-index_documents_action.short_description = 'Index documents for given category'
+        index_documents_in_category.delay(category.id)
+    modeladmin.message_user(
+        request, "Documents indexing has started, please wait for completion"
+    )
+
+
+index_documents_action.short_description = "Index documents for given category"
+
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ('file', 'title', 'category')
     list_filter = ['category', ]
-
     actions = [index_documents_action]
+
 
 class DocumentInline(admin.TabularInline):
     model = Document
     extra = 1  # number of extra forms to display
 
+
 class CorrectionInline(admin.TabularInline):  # New Inline class for Correction
     model = Correction
     extra = 1
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -40,12 +65,63 @@ class CategoryAdmin(admin.ModelAdmin):
 
     def formfield_for_dbfield(self, db_field, **kwargs):
         formfield = super().formfield_for_dbfield(db_field, **kwargs)
-        if db_field.name == 'prompt':
-            formfield.widget = forms.Textarea(attrs={'style': 'font-family:monospace;', 'cols': '120'})
+        if db_field.name == "prompt":
+            formfield.widget = forms.Textarea(
+                attrs={"style": "font-family:monospace;", "cols": "120"}
+            )
         return formfield
-    actions = [index_documents_in_category_action]
 
-@admin.register(Correction) 
+    def download_files(self, request, queryset):
+        # Create a temporary directory to store the files
+        with TemporaryDirectory() as tmp_dir:
+            for category in queryset:
+                documents = Document.objects.filter(category=category)
+                for document in documents:
+                    if document.title:
+                        sanitized_title = sanitize_filename(document.title)
+                        original_extension = os.path.splitext(document.file.name)[1]
+                        new_file_name = f"{sanitized_title}{original_extension}"
+                    else:
+                        new_file_name = document.file.name.split("/")[
+                            -1
+                        ]  # Use the original file name
+
+                    file_path = os.path.join(tmp_dir, new_file_name)
+                    if default_storage.exists(document.file.name):
+                        with default_storage.open(document.file.name, "rb") as f:
+                            file_content = f.read()
+                            with open(file_path, "wb") as local_file:
+                                local_file.write(file_content)
+
+            # Create a temporary file to store the zip
+            zip_file = TemporaryFile()
+
+            with ZipFile(zip_file, "w") as zipfile:
+                # go through each file in the temporary directory and add to zip
+                for root, dirs, files in os.walk(tmp_dir):
+                    for file in files:
+                        zipfile.write(os.path.join(root, file), arcname=file)
+
+            zip_file.seek(0)
+
+            # Create a file response to download the zip
+            response = FileResponse(
+                zip_file, as_attachment=True, filename="documents.zip"
+            )
+
+            return response
+
+    download_files.short_description = "Download documents for selected categories"
+    actions = [
+        index_documents_in_category_action,
+        download_files,
+    ]  # Add the new action to the existing ones
+
+
+@admin.register(Correction)
 class CorrectionAdmin(admin.ModelAdmin):
-    list_display = ('category', 'query', 'answer', 'corrected_at')
-    list_filter = ['category', ]
+    list_display = ("category", "query", "answer", "corrected_at")
+    list_filter = [
+        "category",
+    ]
+
