5366cbbefeeed2ee0e78c98ebf76b5f2ad115ce2
Jérôme Blondon <jerome@blondon.fr>
2023-08-07 14:09:00 +0200

    Fix vector search

--- documents/management/commands/vector_search.py
+++ documents/management/commands/vector_search.py
@@ -1,29 +1,33 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 import qdrant_client
+from documents.anonymize import Anonymizer
 from documents.embedding import get_embedding
 from documents.index import index_documents
 from langchain.vectorstores import Qdrant
+
+from documents.models import Category
+from documents.search import DocumentSearch
 
 class Command(BaseCommand):
     help = 'Search inside vector DB'
 
     def add_arguments(self, parser):
         parser.add_argument('query', type=str, help='The query string')
+        parser.add_argument(
+            "category_slug", type=str, help="Indicates the slug of the category"
+        )
 
     def handle(self, *args, **options):
         query = options['query']        
-        embeddings = get_embedding()
-        url = settings.QDRANT_URL
-        client = qdrant_client.QdrantClient(
-            url=url, prefer_grpc=True
-        )
-        docsearch = Qdrant(
-            client=client, collection_name="documents", 
-            embedding_function=embeddings.embed_query
-        )    
+        category_slug = options["category_slug"]
 
-        res = docsearch.similarity_search_with_score(query, k=4)
-        for doc, score in res:
-            self.stdout.write(f"# {doc.metadata['origin']} -> {score}")
+        self.stdout.write("Category slug: %s" % category_slug)
+
+        query = Anonymizer().anonymize(query)
+        category = Category.objects.get(slug=category_slug)
+
+        document_search = DocumentSearch(category=category)
+        documents = document_search.get_relevant_documents(query)
+        for doc in documents:
             self.stdout.write(doc.page_content)
